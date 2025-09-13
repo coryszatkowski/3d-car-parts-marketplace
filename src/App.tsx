bf53947ae42d67a6ad23afaba5afdc479e81f0df
coryszatkowski <coryszatkowski@gmail.com>
2025-09-13 12:54:15 -0500

    update vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { AuthProvider } from "./contexts/AuthContext";
